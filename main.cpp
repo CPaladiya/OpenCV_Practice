139b4a0741ba570444816756b83eefaea79bc2cf
cpaladiya <chirag.paladiya@hotmail.com>
2021-05-10 22:23:42 -0400

    Feat : Add Image Processing Func

--- main.cpp
+++ main.cpp
@@ -1,82 +1,54 @@
-//#include <opencv2/opencv.hpp> 
-
-//we can include this file and we then dont have to include any other opencv header files
-// however this makes it slower and takes lot of time to compile since cmake has to go and paste entire code here that wont be useful
-
-//so since we only need few header files from opencv we can mention those only here
-
 #include <opencv2/imgcodecs.hpp> //this is for reading files
 #include <opencv2/highgui.hpp> //this is for basic gui
 #include <opencv2/imgproc.hpp> //for image processing
 #include <iostream>
 
-
-
 using namespace cv;
 using namespace std;
 
-//---------------------------Chapter 1 : reading images ----------------------------------//
-
-// this part is for reading/importing the image //
-
-/*
-int main(){
-
-    string path = "sample.jpg"; //path to the image
-    Mat img = imread(path); //Mat is the matrix of numbers, img is the name of that variable and imread will read the image
-
-    //to show the image we can use the command imshow - imageshow
-    //namedWindow("Sample Image", WINDOW_AUTOSIZE); - this window will match the size of the image itself
-    imshow("sample image", img); //this will now show the image but it will close automatically. 
-    //to give it some delay we need to enter the following line
-
-    waitKey(0);//this will make sure that window only closes when we press the close button
-
-}
-*/
-
-//---------------------------Chapter 2 : reading the video ----------------------------------//
-
-/*
-int main(){
-
-    //first like image we need to make a video object
-    string path = "Gossip.mp4"; //path to the video
-    VideoCapture vdo(path); //mew cap
-    Mat img; //here we are initiating the image object that will be stored in while loop with vdo content
-    
-    
-    //since videos are series of frames we need to save all the frames and then play it one by one. 
-    while(true){
-    
-        vdo.read(img); 
-        imshow("Gossip", img); //now one by one we will read frame and show it here on imshow,
-        //note we are not using waitKey(0) since we dont want to pause just play the whole video at one go
-        waitKey(50); //by changing the delay value we can change how fast or slow the video is being played
-    }
-
-}
-*/
-
-//---------------------------Chapter 2 : accessing the webcam ----------------------------------//
+//---------------------------Chapter 2 : Basic functions to operate on image ------------------//
 
 int main(){
 
-    //first like image we need to make a video object, but instaed of path we will provide an id
+    string path = "sample.jpg"; 
+    string path_hand = "Hand.jpg";
+    Mat img = imread(path); 
+    Mat img_hand = imread(path_hand); 
+    Mat imgGray; // here we are defining the output image since we are not modifying the source image
+    Mat imgBlurr, imgBlurr_hand; //to create a blurred image later on
+    Mat imgEdge, imgEdge_hand; //to detect edge later on
+    Mat imgDil, imgEro; //image dialation and image erosion
 
-    VideoCapture vdo(0); //if you only have one camera, thats the camera id 0. if there are more than one, then flipping through 0 
-    //to number of total cameras-1 can help with camera id
+    //-------------1.to covert image in grayscale, we can use command down below
+    cvtColor(img, imgGray, COLOR_BGR2GRAY);
 
-    Mat img; //here we are initiating the image object that will be stored in while loop with vdo content
-    
-    
-    //since videos are series of frames we need to save all the frames and then play it one by one. 
-    while(true){
-    
-        vdo.read(img); 
-        imshow("Gossip", img); //now one by one we will read frame and show it here on imshow,
-        //note we are not using waitKey(0) since we dont want to pause just play the whole video at one go
-        waitKey(1); //the delay here should be 1, just to make sure we are not too slow
-    }
+    //this commands will only show one window at a time
+    imshow("sample image", img);
+    imshow("sample image", imgGray); 
+
+    //to show both image in a single window,follow this link,
+    //https://www.programmersought.com/article/51853081107/
+
+    //---------------2.now lets blurr the image
+    GaussianBlur(img_hand, imgBlurr_hand, Size(3,3),0,0);
+    GaussianBlur(img, imgBlurr, Size(3,3),0,0);
+    imshow("sample image", imgBlurr_hand); 
+
+    //---------------3. Edge detection with kenney edge detector
+    //normally its a common practice to blur image before using edge detection to only find hard edges
+    Canny(imgBlurr, imgEdge, 50, 50);
+    Canny(imgBlurr_hand, imgEdge_hand, 75, 75);
+    imshow("sample image", imgEdge_hand); 
+
+    //--------------4.Dialate and erode, meaning decreasing or increasing thickness of edges, if they are too thin or too thick
+
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(2,2)); //we are making a karnel - or a small matrix that will be used
+    //in dialation of the image, with bigger size we will have much more dialation
+    dilate(imgEdge_hand, imgDil, kernel); //increase the edge thickness
+    imshow("sample image", imgDil); 
+
+
+
+    waitKey(0);
 
 }
