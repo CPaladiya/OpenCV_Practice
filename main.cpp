e3276075fca739b40f065706d9481d9ffb06fc36
cpaladiya <chirag.paladiya@hotmail.com>
2021-05-10 21:11:14 -0400

    Feat : Webcam Access

--- main.cpp
+++ main.cpp
@@ -37,6 +37,7 @@ int main(){
 
 //---------------------------Chapter 2 : reading the video ----------------------------------//
 
+/*
 int main(){
 
     //first like image we need to make a video object
@@ -52,6 +53,30 @@ int main(){
         imshow("Gossip", img); //now one by one we will read frame and show it here on imshow,
         //note we are not using waitKey(0) since we dont want to pause just play the whole video at one go
         waitKey(50); //by changing the delay value we can change how fast or slow the video is being played
+    }
+
+}
+*/
+
+//---------------------------Chapter 2 : accessing the webcam ----------------------------------//
+
+int main(){
+
+    //first like image we need to make a video object, but instaed of path we will provide an id
+
+    VideoCapture vdo(0); //if you only have one camera, thats the camera id 0. if there are more than one, then flipping through 0 
+    //to number of total cameras-1 can help with camera id
+
+    Mat img; //here we are initiating the image object that will be stored in while loop with vdo content
+    
+    
+    //since videos are series of frames we need to save all the frames and then play it one by one. 
+    while(true){
+    
+        vdo.read(img); 
+        imshow("Gossip", img); //now one by one we will read frame and show it here on imshow,
+        //note we are not using waitKey(0) since we dont want to pause just play the whole video at one go
+        waitKey(1); //the delay here should be 1, just to make sure we are not too slow
     }
 
 }
