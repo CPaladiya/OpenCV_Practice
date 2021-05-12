3ecca4fe8928c43d95effb7fd5a8d2fb99bc5482
cpaladiya <chirag.paladiya@hotmail.com>
2021-05-11 20:57:18 -0400

    Feat : Create Blank Image

--- main.cpp
+++ main.cpp
@@ -6,35 +6,30 @@
 using namespace cv;
 using namespace std;
 
-//---------------------------Chapter 3 : resize, crop function ------------------//
+//---------------------------Chapter 4 : Drawings shape on image and adding texts ------------------//
 
 int main(){
 
-    string path_hand = "Hand.jpg";
-    Mat img = imread(path_hand); 
-    Mat ResizeImg, CropImg;
-    
-    cout<< img.size()<<endl; //to know the size of existing image
-    
-    // -------------------------- resize -------------------------//
-    resize(img,ResizeImg, Size(640,480)); //this might make image croocked since we are just resizing it without
-    //concerning with aspect ratio
+    //--------------creating blank image ------------------------//
 
-    //if we just want to scale it down instead then we can do something like this
-    //resize(img,ResizeImg, 0.5,0.5); this will reduce in terms of percentage over x and y axis
-    
-    imshow("Resize Image",ResizeImg);
+    //Here 512 X 512 is the resolution of the image, CV_8UC3 means 8-bit color value unsinged of that would than very
+    //from 0 to  255. Here if it is 8S - signed -127 to 128 approx. and 3C means 3 Channed B, G and R
+    Mat img(512,512, CV_8UC3, Scalar(255,255,255)); //0 means no color and 255 means color with full intensity
 
-    // ----------------------------- cropping image - ROI region of interest -------------------------//
-    
-    Rect roi(400,200,300,300); //rect here is a rectangle data type, x,y and width and height are four parameters
-    // we will put this roi object into our image as the following line suggests
-    //first two is the starting point of the crop and then next two is the diagonal end point
+    //------creating circle on existing image -----------------//
+    circle(img, Point(256,256), 155, Scalar(255,0,0), FILLED); //first point object is center, second is radius, third is color, and
+    //fourth is thickness of the line itself. instead of thickness we can write FILLED keyword to get it filled
 
-    CropImg = img(roi); //in this way we will be able to crop our image
-    
-    imshow("Crop Image",CropImg);
+    //------creating rectanlge --------------------------//
+    rectangle(img, Point(10,10), Point(100,100), Scalar(255,0,0), 3); // here first and second point is xy of first point
+    //xy of diagonal point. This is different than Rect  object we saw before //Rect roi(400,200,300,300);
 
+    //---------------draw a line -----------------------//
+    line(img,Point(124,54), Point(245,54), Scalar(0,255,255),5); //for line we just have ending and starting point
+
+    //------- Adding Characters/texts------------------//
+
+    imshow("Image",img);
     waitKey(0);
 
 }
